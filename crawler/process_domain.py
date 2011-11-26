4ae67cf501f927c7abaf2c08cb0801f5fcacdcde
andrew_kittredge <andrewlkittredge@gmail.com>
2011-11-26 17:25:58 -0500

    Cached scraping.

--- crawler/process_domain.py
+++ crawler/process_domain.py
@@ -1,4 +1,4 @@
-from process_page import process_url
+from process_page import process_url, PageProcessingException
 from corpa import Corpa
 from urlparse import urlparse, urljoin
 import urllib2
@@ -17,11 +17,7 @@ def process_domain(domain, n_gram_builder):
         try:
             page_text, page_urls = process_url(url)
             logging.info('Opened %s' % url)
-        except urllib2.HTTPError:
-            logging.warning('HTTPError opening %s' % url)
-            continue
-        except urllib2.URLError:
-            logging.warning('URLError opening %s' % url)
+        except PageProcessingException:
             continue
         domain_links, neighboring_domains = filter_links(page_urls, domain)
         
@@ -34,7 +30,7 @@ def process_domain(domain, n_gram_builder):
 
 def sanitize_page_path(page_path):
     old_page_path = page_path
-    page_path = urllib.quote(page_path, safe='/')
+    page_path = urllib.quote(page_path, safe='/%')
     if page_path != old_page_path:
         logging.info('%s became %s' % (old_page_path, page_path))
     return page_path
@@ -47,9 +43,13 @@ def filter_links(urls, domain):
         if '@' in url:
             logging.info('%s looks like an email, skipping' % url)
             continue
+            
         o = urlparse(url)
         if not o.netloc or domain in o.netloc:
             page = sanitize_page_path(o.path)
+            if len(page) > 100:
+                logging.info('%s is very long, probably not a good url.' % page)
+                continue 
             domain_pages.add(page)
         else:
             neighboring_domains.append(o.netloc)
--- crawler/process_page.py
+++ crawler/process_page.py
@@ -2,15 +2,51 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 import re
 from preprocessing_utilities import cleanse
+from shove import Shove
+import logging
+
+pages_cache = Shove('file:///Users/andrewkittredge/Source/ai/similar_page/cached_pages')
+
 
 def process_url(url):
-    page = urllib2.urlopen(url)
-    page_soup = BeautifulSoup(page.read())
+    page = get_page_contents(url)
+    page_soup = BeautifulSoup(page)
     page_text = visible_text(page_soup)
     page_text = cleanse(page_text)
     urls = [anchor['href'].strip() for anchor in page_soup.findAll('a') if anchor.has_key('href')]
    
     return page_text, urls
+
+page_failure_token = 'CouldNotOpenPage'
+def get_page_contents(url):
+    if url not in pages_cache:
+        logging.info('Cache miss %s' % url)
+        try:
+            f = urllib2.urlopen(url)
+            try:
+                pages_cache[url] = f.read()
+            except KeyError:
+                logging.warning('KeyError on %s' % url)
+                raise PageProcessingException()
+                
+        except urllib2.HTTPError:
+            logging.warning('HTTPError opening %s' % url)
+            try:
+                pages_cache[url] = page_failure_token
+            except KeyError:
+                logging.warning('KeyError on %s' % url)
+                raise PageProcessingException()
+        except urllib2.URLError:
+            logging.warning('URLError opening %s' % url)
+            try:
+                pages_cache[url] = page_failure_token
+            except KeyError:
+                logging.warning('KeyError on %s' % url)
+                raise PageProcessingException()
+    if pages_cache[url] == page_failure_token:
+        raise PageProcessingException()
+    return pages_cache[url]
+    #return pages_cache.setdefault(url, urllib2.urlopen(url).read())
 
 def visible_text(soup):
     #from http://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
@@ -26,6 +62,9 @@ def visible(element):
         return False
     return True
 
+class PageProcessingException(Exception):
+    pass
+
 
 import unittest
 class TestProcessPage(unittest.TestCase):
@@ -33,7 +72,11 @@ class TestProcessPage(unittest.TestCase):
         text, links = process_url('http://patriotgetaways.com')
         self.assert_(text)
         self.assert_(links)
-        print text, links
         
+    def test_exception_page(self):
+        self.assertRaises(PageProcessingException, process_url, 'http://not a url.com')
+        
+    def test_key_error(self):
+        process_url('http://patriotgetaways.com/pigeon-forge-luxury-cabins/')
 if __name__ == '__main__':
     unittest.main()
