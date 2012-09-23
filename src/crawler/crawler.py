8614404a8721e379db2fab8f95fbbaf6ea06428c
akittredge <andrewlkittredge@gmail.com>
2012-09-22 20:45:13 -0400

    various changes.

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -3,6 +3,8 @@ Created on Dec 26, 2011
 
 @author: akittredge
 '''
+
+
 from frontier import Frontier
 from process_domain import process_domain
 
@@ -26,25 +28,24 @@ class Crawler(object):
             else:
                 for neighbor in neighbors:
                     frontier.put(neighbor, distance + 1)
-            
-                    
-            
         
-    def process_seed_domains(self, seed_domains):
-        neighboring_domains = []
-        records = []
-        for domain in seed_domains:
-            domain_records, domain_neighbors = process_domain(domain)
-            neighboring_domains.extend(domain_neighbors)
-         
-            records.append(domain_records)
-        return records, set(neighboring_domains)
+def process_seed_domains(self, seed_domains):
+    neighboring_domains = []
+    records = []
+    for domain in seed_domains:
+        domain_records, domain_neighbors = process_domain(domain)
+        neighboring_domains.extend(domain_neighbors)
+     
+        records.append(domain_records)
+    return records, set(neighboring_domains)
+
+def build_decision_tree_from_domains(domains):
     
-    
+
 import unittest
 class CrawlerTester(unittest.TestCase):
     def test_set_up(self):
-        domains = ['patriotgetaways.com']
+        domains = ['patriotgetaways.com', 'ivylettings.com']
         crawler = Crawler(scorer = lambda domain : True,
                           domain_processor=process_domain,
                           seed_domains=domains)
